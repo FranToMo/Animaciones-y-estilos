8bb0a66fa452181b0fe46adead1a952118b1eb6d
FranToMo <7787918@alu.murciaeduca.es>
2022-03-04 08:58:45 +0100

    añado el metodo que asigna los valores de los comboBox a los nodos Text

--- src/main/java/es/ideas/chronometer/Controller.java
+++ src/main/java/es/ideas/chronometer/Controller.java
@@ -1,33 +1,55 @@
 package es.ideas.chronometer;
 
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import javafx.scene.text.Text;
 import java.net.URL;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.TreeMap;
 
 public class Controller implements Initializable {
 
-
     @FXML
     private ComboBox<Integer> horasInput, minutosInput, segundosInput;
+    @FXML
+    private Text horasTime, minutosTime, segundosTime;
     Map<Integer, String> numberMap;
+    Integer segundosActuales;
 
+
+    public Integer hmsToSeconds(Integer h, Integer m, Integer s) {
+        Integer hToSeconds = h * 3600;
+        Integer mToSecond = m * 60;
+
+        return hToSeconds + mToSecond + s;
+    }
+
+    public LinkedList<Integer> secondsToHms(Integer segundosActuales) {
+        Integer horas = segundosActuales / 3600;
+        segundosActuales = segundosActuales % 3600;
+        Integer minutos = segundosActuales / 60;
+        Integer segundos = segundosActuales % 60;
+        LinkedList<Integer> respuesta = new LinkedList<>();
+        respuesta.add(horas);
+        respuesta.add(minutos);
+        respuesta.add(segundos);
+        return respuesta;
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        ObservableList<Integer>listaDeHoras = FXCollections.observableArrayList();
-        ObservableList<Integer>listaDeMinutosYSegundos = FXCollections.observableArrayList();
+        ObservableList<Integer> listaDeHoras = FXCollections.observableArrayList();
+        ObservableList<Integer> listaDeMinutosYSegundos = FXCollections.observableArrayList();
 
 
-        for (int i = 0; i <=59 ; i++) {
-            if(0<=i && i<=23){
+        for (int i = 0; i <= 59; i++) {
+            if (0 <= i && i <= 23) {
                 listaDeHoras.add(Integer.valueOf(i));
             }
             listaDeMinutosYSegundos.add(Integer.valueOf(i));
@@ -40,15 +62,24 @@ public class Controller implements Initializable {
         segundosInput.setValue(0);
 
         numberMap = new TreeMap<>();
-        for (Integer i = 0; i <=59 ; i++) {
-                if(0<=i && i<=9){
-                    numberMap.put(i,"0"+i.toString());
-                }else{
-                    numberMap.put(i,i.toString());
-                }
+        for (Integer i = 0; i <= 59; i++) {
+            if (0 <= i && i <= 9) {
+                numberMap.put(i, "0" + i.toString());
+            } else {
+                numberMap.put(i, i.toString());
+            }
         }
-
-
     }
 
+    /**
+     * Asigna el valor seleccionado en cada comboBox
+     * al valor inicial de cada uno de los nodos Text
+     * que forman el cronómetro.
+     */
+    void setOutput(){
+        LinkedList<Integer> actualHms = secondsToHms(segundosActuales);
+        horasTime.setText(numberMap.get(actualHms.get(0)));
+        minutosTime.setText(numberMap.get(actualHms.get(1)));
+        segundosTime.setText(numberMap.get(actualHms.get(2)));
+    }
 }
